0a0b7defac8862c047d85ae1de3d47721c6705cd
tandemic97 <tandemic97@gmail.com>
2022-05-19 23:45:14 +0300

    first commit

--- cache.go
+++ cache.go
@@ -2,22 +2,47 @@ package cache
 
 import "time"
 
+var infinite time.Time = time.Time{}
+
+type cacheValue struct {
+	value    string
+	deadline time.Time
+}
+
 type Cache struct {
+	cache map[string]cacheValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{cache: make(map[string]cacheValue)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	switch {
+	case c.cache[key].deadline != infinite && time.Now().After(c.cache[key].deadline):
+		delete(c.cache, key)
+		return "", false
+	case c.cache[key].deadline != infinite && !time.Now().After(c.cache[key].deadline):
+		return c.cache[key].value, true
+	default:
+		return c.cache[key].value, true
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.cache[key] = cacheValue{value: value, deadline: infinite}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var keys []string
+	for k := range c.cache {
+		if _, ok := c.Get(k); ok {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.cache[key] = cacheValue{value: value, deadline: deadline}
 }
